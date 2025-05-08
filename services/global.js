d3099bc9cbc2557208678dbba079ff10d02af9a7
chensofer <chensofer123@gmail.com>
2025-05-09 02:54:10 +0300

    תיקון עיצובים, מלל תוכן+ רספונסיביות

--- services/global.js
+++ services/global.js
@@ -1,6 +1,5 @@
 function toggleMenu() {
     const navList = document.querySelector('.main-nav ul');
-
     navList.classList.toggle('active');
 }
   
@@ -11,23 +10,11 @@ function toggleDarkMode() {
     body.classList.toggle('dark-mode');
 
     if (body.classList.contains('dark-mode')) {
-        localStorage.setItem('darkMode', 'true');
         darkModeToggle.innerHTML = '☀️';
       } else {
-        localStorage.setItem('darkMode', 'false');
         darkModeToggle.innerHTML = '🌙';
       }
 }
 
-window.addEventListener('DOMContentLoaded', function() {
-    
-    const darkModeToggle = document.getElementById('dark-mode-toggle');
-    
-    if (localStorage.getItem('darkMode') === 'true') {
-        document.body.classList.add('dark-mode');
-        darkModeToggle.innerHTML = '☀️';
-      } else {
-        darkModeToggle.innerHTML = '🌙'; 
-      }
-  });
+
   
